4c237b416afe6d71f6118391a0335f97660b4566
Office <other@Mahirs-MacBook-Air.local>
2025-03-29 16:51:19 +0530

    Final changes

--- medibot.py
+++ medibot.py
@@ -8,30 +8,31 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint
 
+#import functions
+# from connect_memory_with_LLM import set_custom_prompt,llm,CUSTOM_PROMPT_TEMPLATE
+
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
+HF_TOKEN=os.environ.get("HF_TOKEN")
+
+embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 DB_FAISS_PATH="vectorstore/db_faiss"
 @st.cache_resource
-def get_vectorstore():
-    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+def load_FAISS():
     db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
     return db
 
+llm=HuggingFaceEndpoint(
+    repo_id=HUGGINGFACE_REPO_ID,
+    temperature=0.5,
+    model_kwargs={"token" : HF_TOKEN , "max_length":"512"}
+)
+
 def set_custom_prompt(custom_prompt_template):
-    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context","question"])
     return prompt
-
-
-def load_llm(huggingface_repo_id, HF_TOKEN):
-    llm=HuggingFaceEndpoint(
-        repo_id=huggingface_repo_id,
-        temperature=0.5,
-        model_kwargs={"token":HF_TOKEN,
-                      "max_length":"512"}
-    )
-    return llm
-
 
 def main():
     st.title("Ask Chatbot")
@@ -46,8 +47,7 @@ def main():
 
     if prompt:
         st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role':'user', 'content': prompt})
-
+        st.session_state.messages.append({'role':'user', 'content': prompt})  
         CUSTOM_PROMPT_TEMPLATE = """
             Use the pieces of information provided in the context to answer user's question.
             If you dont know the answer, just say that you dont know, dont try to make up an answer. 
@@ -59,15 +59,13 @@ def main():
             Start the answer directly. No small talk please.
             """
         
-        HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
-        HF_TOKEN=os.environ.get("HF_TOKEN")
         try: 
-            vectorstore=get_vectorstore()
+            vectorstore=load_FAISS()
             if vectorstore is None:
                 st.error("Failed to load the vector store")
 
             qa_chain=RetrievalQA.from_chain_type(
-                llm=load_llm(huggingface_repo_id=HUGGINGFACE_REPO_ID, HF_TOKEN=HF_TOKEN),
+                llm=llm,
                 chain_type="stuff",
                 retriever=vectorstore.as_retriever(search_kwargs={'k':3}),
                 return_source_documents=True,
@@ -76,9 +74,6 @@ def main():
 
             response=qa_chain.invoke({'query':prompt})
 
-            # result=response["result"]
-            # source_documents=response["source_documents"]
-            # result_to_show=result+"\nSource Docs:\n"+str(source_documents)
             result = response["result"]
             source_documents = response["source_documents"]
 
